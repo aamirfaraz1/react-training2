4cb517fb926bef5630802ced8188a2c096006118
aamirfaraz1 <aamirfaraz1@gmail.com>
2022-09-23 12:01:39 +0530

    hello world

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <br></br>
+      <p1>Name: Aamir Faraz</p1>
+      <br></br>
+      <br></br>
+      <p2>Age: 22</p2>
+      <br></br>
+      <br></br>
+      <p3>DoB: 22/05/2000</p3>
+    </div>
+  );
+}
+
+export default App;
